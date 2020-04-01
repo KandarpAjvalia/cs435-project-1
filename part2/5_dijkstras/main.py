514ac98ca895a49fda6ba6638d1d43b8483b63af
kandarpajvalia <kandarpa9@gmail.com>
2020-04-01 01:45:10 -0400

    dijkstra done

--- part2/5_dijkstras/main.py
+++ part2/5_dijkstras/main.py
@@ -1,5 +1,9 @@
 import random
+import sys
 from weightedGraph import WeightedGraph
+import heapq
+import json
+
 
 class Main:
 
@@ -22,17 +26,69 @@ class Main:
         for i in range(n):
             graph.addNode(i)
 
-        for i in  range(n - 1):
+        for i in range(n - 1):
             graph.addWeightedEdge(i, i + 1, 1)
 
         return graph
 
-if __name__=='__main__':
+    @staticmethod
+    def dijkstras(graph, start):
+        nodeDistanceMap = {}
+        weightNodeTupleList = [(0, start)]
+        valsInHeap = set()
+        valsInHeap.add(start)
+        allNodes = set()
+        allNodes.add(start)
+        while weightNodeTupleList:
+            heapq.heapify(weightNodeTupleList)
+            weightNodeTuple = heapq.heappop(weightNodeTupleList)
+            currWeight = weightNodeTuple[0]
+            curr = weightNodeTuple[1]
+            nodeDistanceMap[curr] = currWeight
+            valsInHeap.remove(curr)
+
+            for node, weight in graph.getNeighbors(curr).items():
+                if node not in allNodes:
+                    allNodes.add(node)
+                    weightNodeTupleList.append((sys.maxsize, node))
+                    valsInHeap.add(node)
+
+                if node not in valsInHeap:
+                    continue
+
+                newDistance = currWeight + weight
+
+                index = Main.findNode(weightNodeTupleList, node)
+                if newDistance < weightNodeTupleList[index][0]:
+                    weightNodeTupleList[index] = (newDistance, node)
+
+        return nodeDistanceMap
+
+    @staticmethod
+    def findNode(tupleList, node):
+        for i in range(len(tupleList)):
+            if tupleList[i][1] == node:
+                return i
+
+
+if __name__ == '__main__':
     print()
-    print('5 c -------------------- Random Complete Weighted Graph')
-    graph = Main.createRandomCompleteWeightedGraph(5)
+    print('5 d -------------------- Random Complete Weighted Graph')
+    graph = Main.createRandomCompleteWeightedGraph(6)
     print(graph)
+
+    print()
+    print('5 e i -------------------- Djikstra on Random Complete Weighted Graph, start = 1')
+    distancesFromNode = Main.dijkstras(graph, 1)
+    print(json.dumps(distancesFromNode, indent=4, sort_keys=True))
+
     print()
     print('5 d -------------------- Create LinkedList')
-    graph = Main.createLinkedList(5)
-    print(graph)
+    linkedList = Main.createLinkedList(5)
+    print(linkedList)
+
+    print()
+    print('5 e ii -------------------- Djikstra on Linked List, start = 0')
+    distancesFromNode = Main.dijkstras(linkedList, 0)
+    print(json.dumps(distancesFromNode, indent=4, sort_keys=True))
+    print()
